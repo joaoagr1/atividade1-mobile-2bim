c689ac9cca36eba60a4d8c03611be95f1db61dbe
joaoagr1 <joao.rollo1@gmail.com>
2024-10-14 22:21:26 -0300

    adicionando base para projeto

---
+++ mobile/android/app/src/main/kotlin/com/example/atvmobile/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.atvmobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ mobile/lib/service/abstract_api.dart
@@ -0,0 +1,42 @@
+import 'package:http/http.dart' as http;
+
+abstract class AbstractApi {
+  final String urlLocalhost = 'http://localhost:3000';
+  String _recurso;
+
+  AbstractApi(this._recurso);
+
+  Future<String> getAll() async {
+    var response = await http.get(Uri.parse("$urlLocalhost/$_recurso"));
+    return response.body;
+  }
+
+
+  Future<String> getById(String id) async {
+    var response = await http.get(Uri.parse("$urlLocalhost/$_recurso/$id"));
+    return response.body;
+  }
+
+  Future<String> post(String body) async {
+    var response = await http.post(
+      Uri.parse("$urlLocalhost/$_recurso"),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: body,
+    );
+    return response.body;
+  }
+
+  Future<String> put(String id, String body) async {
+    var response = await http.put(
+      Uri.parse("$urlLocalhost/$_recurso/$id"),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: body,
+    );
+    return response.body;
+  }
+
+}
